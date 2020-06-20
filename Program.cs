691a41fe431b0a9633092223037c58f70bd6ec21
Kim Hang <1654052033hang@ou.edu.vn>
2020-06-20 15:51:05 +0700

    chạy

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace QuanLyCuaHangNuocNgot
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmDoiMatKhau());
+            Application.Run(new frmLogin());
         }
     }
 }
