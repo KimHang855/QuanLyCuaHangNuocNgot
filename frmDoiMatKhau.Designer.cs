330b52c1ca91c7d22f707f644dc9797c0be617c6
Kim Hang <1654052033hang@ou.edu.vn>
2020-06-20 15:19:28 +0700

    thiết kế giao diện reset mật khẩu

--- frmDoiMatKhau.Designer.cs
+++ frmDoiMatKhau.Designer.cs
@@ -28,11 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtXacNhanMK = new System.Windows.Forms.TextBox();
+            this.txtMK = new System.Windows.Forms.TextBox();
+            this.txtTentk = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbMatKhau = new System.Windows.Forms.Label();
+            this.lbTaiKhoan = new System.Windows.Forms.Label();
+            this.btThoat = new System.Windows.Forms.Button();
+            this.btReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(110, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 29);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "RESET PASSWORD";
+            // 
+            // txtXacNhanMK
+            // 
+            this.txtXacNhanMK.Location = new System.Drawing.Point(218, 209);
+            this.txtXacNhanMK.Margin = new System.Windows.Forms.Padding(4);
+            this.txtXacNhanMK.Name = "txtXacNhanMK";
+            this.txtXacNhanMK.Size = new System.Drawing.Size(193, 22);
+            this.txtXacNhanMK.TabIndex = 12;
+            // 
+            // txtMK
+            // 
+            this.txtMK.Location = new System.Drawing.Point(218, 161);
+            this.txtMK.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMK.Name = "txtMK";
+            this.txtMK.Size = new System.Drawing.Size(193, 22);
+            this.txtMK.TabIndex = 10;
+            // 
+            // txtTentk
+            // 
+            this.txtTentk.Location = new System.Drawing.Point(218, 113);
+            this.txtTentk.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTentk.Name = "txtTentk";
+            this.txtTentk.Size = new System.Drawing.Size(193, 22);
+            this.txtTentk.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(24, 213);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 20);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Xác nhận mật khẩu";
+            // 
+            // lbMatKhau
+            // 
+            this.lbMatKhau.AutoSize = true;
+            this.lbMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbMatKhau.Location = new System.Drawing.Point(75, 162);
+            this.lbMatKhau.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbMatKhau.Name = "lbMatKhau";
+            this.lbMatKhau.Size = new System.Drawing.Size(109, 20);
+            this.lbMatKhau.TabIndex = 13;
+            this.lbMatKhau.Text = "Mật khẩu mới";
+            // 
+            // lbTaiKhoan
+            // 
+            this.lbTaiKhoan.AutoSize = true;
+            this.lbTaiKhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbTaiKhoan.Location = new System.Drawing.Point(71, 117);
+            this.lbTaiKhoan.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbTaiKhoan.Name = "lbTaiKhoan";
+            this.lbTaiKhoan.Size = new System.Drawing.Size(109, 20);
+            this.lbTaiKhoan.TabIndex = 11;
+            this.lbTaiKhoan.Text = "Tên tài khoản";
+            // 
+            // btThoat
+            // 
+            this.btThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btThoat.Location = new System.Drawing.Point(254, 277);
+            this.btThoat.Margin = new System.Windows.Forms.Padding(4);
+            this.btThoat.Name = "btThoat";
+            this.btThoat.Size = new System.Drawing.Size(117, 42);
+            this.btThoat.TabIndex = 16;
+            this.btThoat.Text = "Thoát";
+            this.btThoat.UseVisualStyleBackColor = true;
+            this.btThoat.Click += new System.EventHandler(this.btThoat_Click);
+            // 
+            // btReset
+            // 
+            this.btReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btReset.Location = new System.Drawing.Point(86, 277);
+            this.btReset.Margin = new System.Windows.Forms.Padding(4);
+            this.btReset.Name = "btReset";
+            this.btReset.Size = new System.Drawing.Size(117, 42);
+            this.btReset.TabIndex = 15;
+            this.btReset.Text = "Reset";
+            this.btReset.UseVisualStyleBackColor = true;
+            this.btReset.Click += new System.EventHandler(this.btReset_Click);
+            // 
+            // frmDoiMatKhau
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 403);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtXacNhanMK);
+            this.Controls.Add(this.txtMK);
+            this.Controls.Add(this.txtTentk);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbMatKhau);
+            this.Controls.Add(this.lbTaiKhoan);
+            this.Controls.Add(this.btThoat);
+            this.Controls.Add(this.btReset);
+            this.Name = "frmDoiMatKhau";
             this.Text = "frmDoiMatKhau";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtXacNhanMK;
+        private System.Windows.Forms.TextBox txtMK;
+        private System.Windows.Forms.TextBox txtTentk;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbMatKhau;
+        private System.Windows.Forms.Label lbTaiKhoan;
+        private System.Windows.Forms.Button btThoat;
+        private System.Windows.Forms.Button btReset;
     }
 }
