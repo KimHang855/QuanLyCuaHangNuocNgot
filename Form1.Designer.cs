5116aec8b65d12b882d1e24f31fdc291142d54a5
Kim Hang <1654052033hang@ou.edu.vn>
2020-06-20 10:18:07 +0700

    thiết kế giao diện form chính

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,27 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.mnuSP = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuNCC = new System.Windows.Forms.ToolStripMenuItem();
             this.mnuDT = new System.Windows.Forms.ToolStripMenuItem();
-            this.mnuTT = new System.Windows.Forms.ToolStripMenuItem();
             this.mnuThoat = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.mnuSP,
-            this.mnuDT,
-            this.mnuTT,
-            this.mnuThoat});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
             // 
             // mnuSP
             // 
@@ -57,17 +45,19 @@
             this.mnuSP.Text = "Quản lý sản phẩm";
             this.mnuSP.Click += new System.EventHandler(this.quảnLýSảnPhẩmToolStripMenuItem_Click);
             // 
+            // mnuNCC
+            // 
+            this.mnuNCC.Name = "mnuNCC";
+            this.mnuNCC.Size = new System.Drawing.Size(104, 24);
+            this.mnuNCC.Text = "Quản lý NCC";
+            this.mnuNCC.Click += new System.EventHandler(this.mnuNCC_Click);
+            // 
             // mnuDT
             // 
             this.mnuDT.Name = "mnuDT";
-            this.mnuDT.Size = new System.Drawing.Size(148, 24);
-            this.mnuDT.Text = "Báo cáo Doanh thu";
-            // 
-            // mnuTT
-            // 
-            this.mnuTT.Name = "mnuTT";
-            this.mnuTT.Size = new System.Drawing.Size(95, 24);
-            this.mnuTT.Text = "Thanh toán";
+            this.mnuDT.Size = new System.Drawing.Size(146, 24);
+            this.mnuDT.Text = "Báo cáo doanh thu";
+            this.mnuDT.Click += new System.EventHandler(this.mnuDT_Click);
             // 
             // mnuThoat
             // 
@@ -76,19 +66,45 @@
             this.mnuThoat.Text = "Thoát";
             this.mnuThoat.Click += new System.EventHandler(this.mnuThoat_Click);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuSP,
+            this.mnuNCC,
+            this.mnuDT,
+            this.mnuThoat});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::QuanLyCuaHangNuocNgot.Properties.Resources._52;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 422);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
             this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Quản lý cửa hàng nước ngọt";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -96,11 +112,12 @@
 
         #endregion
 
-        private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem mnuSP;
+        private System.Windows.Forms.ToolStripMenuItem mnuNCC;
         private System.Windows.Forms.ToolStripMenuItem mnuDT;
-        private System.Windows.Forms.ToolStripMenuItem mnuTT;
         private System.Windows.Forms.ToolStripMenuItem mnuThoat;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
